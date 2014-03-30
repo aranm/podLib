e30ed9872ba41d73c4636cc51fce2fc6ac90b010
Aran Mulholland <aranmulholland@gmail.com>
2014-03-30 20:39:32 +1100

    Initial commit

---
+++ PodLib/PodLib/PodLib.h
@@ -0,0 +1,13 @@
+//
+//  PodLib.h
+//  PodLib
+//
+//  Created by Aran Mulholland on 30/03/2014.
+//  Copyright (c) 2014 Aran Mulholland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodLib : NSObject
+
+@end
---
+++ PodLib/PodLibTests/PodLibTests.m
@@ -0,0 +1,34 @@
+//
+//  PodLibTests.m
+//  PodLibTests
+//
+//  Created by Aran Mulholland on 30/03/2014.
+//  Copyright (c) 2014 Aran Mulholland. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PodLibTests : XCTestCase
+
+@end
+
+@implementation PodLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
